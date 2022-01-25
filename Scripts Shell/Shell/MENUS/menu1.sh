1ee9046de56ec0e2edcbef7c008c5c5e77155648
aavp13 <a_alvvelpar@iestriana.com>
2022-01-25 13:03:53 +0100

    scripteo

---
+++ Scripts Shell/Shell/MENUS/menu1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+read -p "Introduzca un mensaje" mensaje
+dialog --title "Ejemplo" --msgbox "$mensaje" 10 40
---
+++ Scripts Shell/Shell/MENUS/menu2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dialog --title "Nombre" --inputbox "Introduzca su Nombre" 10 40 2>/tmp/aux.txt
+nombre=$(cat /tmp/aux.txt)
+dialog --title "Nombre" --msgbox "el nombre introducido es:\n$nombre" 10 40
+sleep 2
+clear
