8fc12bf03ab9f54b07fef9e0540429a1669cd75f
aavp13 <a_alvvelpar@iestriana.com>
2022-01-26 23:40:51 +0100

    aasd

--- Scripts Shell/Shell/MENUS/menu2.sh
+++ Scripts Shell/Shell/MENUS/menu2.sh
@@ -3,5 +3,5 @@
 dialog --title "Nombre" --inputbox "Introduzca su Nombre" 10 40 2>/tmp/aux.txt
 nombre=$(cat /tmp/aux.txt)
 dialog --title "Nombre" --msgbox "el nombre introducido es:\n$nombre" 10 40
-sleep 2
+sleep 1
 clear
