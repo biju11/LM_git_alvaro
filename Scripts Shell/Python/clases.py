ca397a777605faeac8334c3de0bd33199e9f4c9f
aavp13 <a_alvvelpar@iestriana.com>
2022-01-31 12:04:55 +0100

    python

---
+++ Scripts Shell/Python/clases.py
@@ -0,0 +1,15 @@
+class __init__(object):
+    def __init__(self,ancho,alto,color):
+        self.ancho=ancho
+        self.alto=alto
+        self.color=color
+        self.estado="cerrada"
+    def abrir(self):
+        self.estado="abierta"
+    def cerrar(self)
+        self.estado="cerrada"
+    def mostrar(self):
+        print("ancho"+str(ancho))
+        print("alto"+str(alto))
+        print("estado"+str(estado))
+    
