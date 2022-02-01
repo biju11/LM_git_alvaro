d39d99651415b16ca3d12b6d62c17229a9a7726a
aavp13 <a_alvvelpar@iestriana.com>
2022-02-01 10:31:16 +0100

    asdfgadfg

--- Scripts Shell/Python/clases_ejemploventana.py
+++ Scripts Shell/Python/clases_ejemploventana.py
@@ -1,4 +1,4 @@
-class __init__(object):
+class ventana:
     def __init__(self,ancho,alto,color):
         self.ancho=ancho
         self.alto=alto
@@ -9,7 +9,22 @@ class __init__(object):
     def cerrar(self):
         self.estado="cerrada"
     def mostrar(self):
-        print("ancho"+str(self.ancho))
-        print("alto"+str(self.alto))
-        print("estado"+str(self.estado))
-    
+        print("ancho"+str(": ")+str(self.ancho))
+        print("alto"+str(": ")+str(self.alto))
+        print("estado"+str(": ")+str(self.estado))
+        print("color"+ str(": ")+str(self.color))
+    def cambio(self):
+        self.color=input("Nuevo color:\n>>> ")
+        self.ancho=int(input("Nuevo ancho:\n>>> "))
+        self.alto=int(input("Nuevo alto:\n>>> "))
+        est=input("Nuevo estado:\n>>> ")
+        if est.lower()=="abierta":
+            self.estado="abierta"
+        elif est.lower()=="cerrada":
+            self.estado="cerrada"
+        else:
+            print("el estado se conservara en: "+str(self.estado))
+a=ventana(1000,2000,"blanca")
+a.mostrar()
+a.cambio()
+a.mostrar()
