f08c77fa10d6e424f53fbe25c51b63da97bb2e1a
aavp13 <a_alvvelpar@iestriana.com>
2022-02-09 01:16:49 +0100

    bash

--- Scripts ISO/bash/MENUS/el.sh
+++ Scripts ISO/bash/MENUS/el.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+#recibe parametros de esta forma /el.sh 5 6 3 4
 #este script tiene que extraer las lineas elegidas por el usuario de un fichero de texto
-
-
+# '$#' recoge el numero de parametros introducidos
+#seguramente haya que usar un exec
 #bucle while [ $archivo_en_variable ]
-#if para extraer la linea y escribirla en un fichero aux.txt
+#if y elif para extraer las lineas y escribirla en un fichero aux.txt
 #
 #
 #
 #
 #
-#
-#
-#
-#
+
+read -p "Escriba la ruta exacta del fichero:    " fichero
+sleep 1
+echo -e "\r"
+cat $fichero>aux.txt
+exec 9<aux.txt
+read -u 9 linea
+while [ '$linea' ]
+    do
+        echo "$linea"
+        read -u 9 linea
+    done
+
+exec 9<&-
--- Scripts ISO/bash/MENUS/infopc.sh
+++ Scripts ISO/bash/MENUS/infopc.sh
@@ -8,8 +8,12 @@ ram=$(cat /tmp/ram.txt)
 cat /proc/partitions >/tmp/particiones.txt
 part=$(cat /tmp/particiones.txt)
 
-#cat /proc/uptime >/tmp/tiempo.txt
-#cat /proc/modules|cut -f1 -d' ' >/tmp/mods.txt
-#cat /proc/devices
+cat /proc/uptime| cut -f1 -d' '>/tmp/tiempo.txt
+let tiempo=$(cat /proc/uptime |cut -f1 -d' ')%60
+
+cat /proc/modules|cut -f1 -d' ' >/tmp/mods.txt
+mods=$(cat /tmp/mods.txt)
+
+cat /proc/devices
     #text dialog --infobox "$almacenar_fichero" 0 0 ; sleep 5
-dialog --infobox "$proc" 50 100
+    dialog --infobox "$proc" 50 100
