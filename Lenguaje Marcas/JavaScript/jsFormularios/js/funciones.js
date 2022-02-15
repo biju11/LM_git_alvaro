d506f5da44e0143a02568525750593810f427c7a
aavp13 <a_alvvelpar@iestriana.com>
2022-02-15 14:35:40 +0100

    sDFSDFSADF

--- Lenguaje Marcas/JavaScript/jsFormularios/js/funciones.js
+++ Lenguaje Marcas/JavaScript/jsFormularios/js/funciones.js
@@ -1,16 +1,59 @@
 function validar(campo) {
-    //var nombre = document.getElementById("nombre");
-    //alert(nombre.value);
-
-
-
-
-    // "|| es OR "
-    // "&& es AND " 
-    if (campo.value == null || campo.value == "" || campo.value.lenght == 0) {
-        campo.style="border: 2px solid red";
+    //var campo = document.getElementById("nombre");
+    //alert(campo.value);
+    if (campo.value == null || campo.value == "" || campo.value.length == 0) {
+        campo.style = "border: 3px solid red";
         campo.focus();
+        muestraError("spnombre","ERROR: CAMPO VACIO")
     } else {
-        campo.style= ""; 
+        campo.style = "";
+        borrarError("spnombre")
     }
+}
+
+function muestraError(campo,mensaje) {
+    document.getElementById(campo).innerHTML=mensaje
+}
+
+function borrarError(mensajer) {
+    document.getElementById(mensajer).innerHTML = ""
+}
+
+function validarFormulario() {
+    //variable que contendrá errores
+    var msgError="";
+    //campos a validar
+    var nombre = document.getElementById("nombre");
+    if (nombre.value == null || nombre.value.length == 0) {
+        msgError= msgError + "<li>Error:    El Nombre está Vacío</li>";
+    }
+    var zipcode=document.getElementById("zipcode");
+    if (zipcode.value == null || zipcode.value.length == 0) {
+        msgError = msgError + "<li>Error:    El Código Postal es Incorrecto</li>";
+    } else if (zipcode.value.length != 5) {
+        msgError = msgError + "<li>Error:    El Código Postal es Incorrecto</li>";
+    }
+    var tel = document.getElementById("tlf");
+    if (tel.value == null || tel.value.length == 0) {
+        msgError=msgError+"<li>Error:    El Teléfono está Vacío</li>";
+    } else if (tel.value.length != 9) {
+        msgError=msgError+"<li>Error:    El Teléfono no tiene 9 caracteres</li>";
+    } else if (!tel.value.startWith(9) && !tel.value.startWith(6)) {
+        msgError=msgError+"<li>Error:    El Teléfono no comienza ni por 9 ni 6</li>";
+    }
+
+
+
+    //verif
+    if (msgError.value == 0) {
+        return true; 
+    } else {
+        document.getElementById("f_error").style.visibility = "visible";
+        muestraError("sperror", msgError);
+        return false;
+    }
+    
+}
+function mostrarEdad(campoEdad) {
+    document.getElementById("edad_val").innerHTML = campoEdad.value;
 }
