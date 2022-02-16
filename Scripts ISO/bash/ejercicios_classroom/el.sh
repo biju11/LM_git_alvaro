bbca3a0e570644c1d903280c46104ffd2b36748e
aavp13 <a_alvvelpar@iestriana.com>
2022-02-16 20:25:14 +0100

    tgsdfgdafg

---
+++ Scripts ISO/bash/ejercicios_classroom/el.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#este script tiene que extraer las lineas elegidas por el usuario de un fichero de texto
+
+
+#bucle while [ $archivo_en_variable ]
+#if para extraer la linea y escribirla en un fichero aux.txt
+#
+#
+#
+#
+#
+#
+#
+#
+#
