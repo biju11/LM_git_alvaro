e2ceb28ec98de0afb26d5345170f05780ff431ea
aavp13 <a_alvvelpar@iestriana.com>
2022-03-11 11:01:59 +0100

    dfhcxxfgd

--- Lenguaje Marcas/SimulacionSwitch/js/funcswitch.js
+++ Lenguaje Marcas/SimulacionSwitch/js/funcswitch.js
@@ -1,10 +1,36 @@
-var color="";
-var num="";
+function myFunction() {
+    document.getElementById("demo").style.background.color = "red";
+  }
 
-function boca() {
-    
-}
 
-function pinta() {
-    
-}
+
+
+
+
+
+
+  function miFunc(a) {
+    document.getElementById("demo").style.color = a;
+   }
+   
+    function activa(b) {
+    document.getElementById(b).style.backgroundColor = document.getElementById("demo").style.color;
+   }
+   
+   function graba(b) {
+  /* 
+   set fso = CreateObject("Scripting.FileSystemObject"); 
+     set s   = fso.CreateTextFile(b, True);
+  
+     //var firstName = document.getElementById('FirstName');
+    // var lastName  = document.getElementById('lastName');
+  
+     s.writeline("verde" );
+     s.writeline("azult");
+  
+     s.writeline("-----------------------------");
+     s.Close();
+     */
+     alert('grabado');
+     
+    }
