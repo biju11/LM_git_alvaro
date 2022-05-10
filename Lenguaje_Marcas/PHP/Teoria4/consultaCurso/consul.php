bce56b024b720f97e50b74d6f19e5f9c34e2b9a8
aavp13 <a_alvvelpar@iestriana.com>
2022-05-10 14:34:23 +0200

    php5

---
+++ Lenguaje_Marcas/PHP/Teoria4/consultaCurso/consul.php
@@ -0,0 +1,18 @@
+<?php
+            $credents = mysqli_connect("localhost","root","usuario.1234","cursophp") or die("Problemas de Conexion");
+            $
+            $sql = "SELECT idAlumno, nombre, mail, codigocurso FROM alumnos";
+            $registros = mysqli_query($credents,$sql) or die("Problema en la consulta " . mysqli_error($credents));
+
+
+            echo"<table class='table table-stripped'>";
+            echo"<thead><th>ID Alumno</th><th>Nombre del Alumno</th><th>E-Mail</th></thead>";
+            while ($reg=mysqli_fetch_array($registros)){
+                echo "<tr>";
+                echo "<td>".$reg['idAlumno']."</td>";
+                echo "<td>".$reg['nombre']."</td>";
+                echo "<td>".$reg['mail']."</td>";
+                echo "</tr>";
+            }
+            echo"</table>";
+?>
