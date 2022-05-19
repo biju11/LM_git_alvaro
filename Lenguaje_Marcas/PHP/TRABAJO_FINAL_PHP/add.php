8eb1b1ca735941aebde8718390a75d70bb1cc7fa
aavp13 <a_alvvelpar@iestriana.com>
2022-05-19 14:41:05 +0200

    madre mia

--- Lenguaje_Marcas/PHP/TRABAJO_FINAL_PHP/add.php
+++ Lenguaje_Marcas/PHP/TRABAJO_FINAL_PHP/add.php
@@ -1,6 +1,7 @@
 <?php
+$conexion = mysqli_connect("localhost", "root", "usuario.1234", "proyectophp") or die("Problemas de Conexión");
 
-$mision = trim(htmlspecialchars($_REQUEST["n_mision"], ENT_QUOTES, "UTF-8"));
+$mision = trim(htmlspecialchars($_REQUEST["mision"], ENT_QUOTES, "UTF-8"));
 $nave = trim(htmlspecialchars($_REQUEST["nave"], ENT_QUOTES, "UTF-8"));
 $fecha = trim(htmlspecialchars($_REQUEST["fecha"], ENT_QUOTES, "UTF-8"));
 
@@ -8,13 +9,12 @@ $comandante = trim(htmlspecialchars($_REQUEST["comandante"], ENT_QUOTES, "UTF-8"
 $nacion = trim(htmlspecialchars($_REQUEST["nacion"], ENT_QUOTES, "UTF-8"));
 $edad = trim(htmlspecialchars($_REQUEST["edad"], ENT_QUOTES, "UTF-8"));
 
-$conexion = mysqli_connect("localhost", "root", "usuario.1234", "proyectophp") or die("Problemas de Conexión");
-$sql = "INSERT INTO integrantes(nom_ape, nacion, edad,ID_tripulante) VALUES ('$comandante','$nacion','$edad',NULL)";
-mysqli_query($conexion, $sql) or die("Error en el insert del Integrante: ".mysqli_error($conexion));
-$sql = "INSERT INTO mision(nombre,vehiculo,fecha_inicio,ID_tripulante) SELECT ('$mision','$nave','$fecha',integrantes.ID_tripulante) FROM integrantes WHERE nom_ape LIKE '%$comandante%'";
-mysqli_query($conexion, $sql) or die("Error en el insert de la Mision: ".mysqli_error($conexion));
+
+
+$sqlA="INSERT INTO integrantes(nom_ape,nacion,edad) VALUES ('$comandante','$nacion','$edad');
+       INSERT INTO mision(nombre,vehiculo,fecha_inicio,ID_tripulante) SELECT ('$mision','$nave','$fecha',NULL);";
+
+mysqli_multi_query($conexion, $sqlA) or die("Error en el insert del Integrante: ".mysqli_error($conexion));
 mysqli_close($conexion);
-
-
 print "<h2>Mision registrada con exito!</h2>"
 ?>
